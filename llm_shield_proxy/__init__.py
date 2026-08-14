276225cefffe2b6c66333a85267b9ccf580ac4ad
Ninad Phalak <ninadphalak@gmail.com>
2026-08-14 11:30:13 -0500

    chore: bump version to 1.0.15, update README and packaging

--- llm_shield_proxy/__init__.py
+++ llm_shield_proxy/__init__.py
@@ -6,7 +6,7 @@ from llm_shield_proxy.pii_engine import PIIEngine, pii_engine
 from llm_shield_proxy.streaming import SSERehydrationBuffer, rehydrate_sse_stream
 from llm_shield_proxy.audit import AuditLogger
 
-__version__ = "1.0.14"
+__version__ = "1.0.15"
 
 __all__ = [
     "settings",
