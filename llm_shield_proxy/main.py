a51cec223c93912d3d026be9bc8cc248858c54a1
Ninad Phalak <ninadphalak@gmail.com>
2026-08-14 21:18:48 -0500

    release: bump version to v1.0.17

--- llm_shield_proxy/main.py
+++ llm_shield_proxy/main.py
@@ -36,7 +36,7 @@ from llm_shield_proxy.pii_engine import pii_engine
 from llm_shield_proxy.streaming import rehydrate_sse_stream
 from llm_shield_proxy.vault import RedisVaultStore, vault_store
 
-APP_VERSION = "1.0.16"
+APP_VERSION = "1.0.17"
 
 
 @lru_cache(maxsize=1024)
